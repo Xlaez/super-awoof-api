ed4177fcebeb75051ddf362e2fb7e13edc9f4380
Utee <utibeabasiekong2@gmail.com>
2024-09-26 11:47:10 +0100

    update:

--- src/components/system/system.component.ts
+++ src/components/system/system.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@dolphjs/dolph/decorators";
 import { SystemController } from "./system.controller";
 import { SystemService } from "./system.service";
+import { WalletService } from "../wallet/wallet.service";
 
 @Component({
   controllers: [SystemController],
