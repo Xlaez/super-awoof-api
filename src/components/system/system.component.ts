04cfd42affc7d4a4affe16b0766fdc8f3a6532e6
Utee <utibeabasiekong2@gmail.com>
2024-09-26 11:46:34 +0100

    update: fix error with undefined service

--- src/components/system/system.component.ts
+++ src/components/system/system.component.ts
@@ -1,10 +1,9 @@
 import { Component } from "@dolphjs/dolph/decorators";
 import { SystemController } from "./system.controller";
 import { SystemService } from "./system.service";
-import { AccountService } from "../account/account.service";
 
 @Component({
   controllers: [SystemController],
-  services: [SystemService, AccountService],
+  services: [SystemService, WalletService],
 })
 export class SystemComponent {}
